d983c3b8203553f5fd84d444070b4ac5c495ffad
cguanzon <cguanzon@instructure.com>
2016-02-22 14:32:13 -0700

    2

--- spec/mountain_bike_spec.rb
+++ spec/mountain_bike_spec.rb
@@ -1,21 +1,21 @@
 require 'rspec'
 require_relative '../classes/mountain_bike'
 
-# # 0
-# describe 'MountainBike' do
-#   it 'creates a rigid type with a 2.5 tire width' do
-#     bike = MountainBike.new(:type_code => :rigid, :tire_width => 2.5)
-#     expect(bike.off_road_ability).to eq 1.5
-#     expect(bike.price).to eq 315.0
-#   end
-#
-#   it 'creates a front suspension bike with tire width 2 and front fork travel 3' do
-#     bike = MountainBike.new(:type_code => :front_suspension, :tire_width => 2,
-#                              :front_fork_travel => 3)
-#     expect(bike.off_road_ability).to eq 2.7
-#     expect(bike.price).to eq 515.0
-#   end
-# end
+# 0
+describe 'MountainBike' do
+  it 'creates a rigid type with a 2.5 tire width' do
+    bike = MountainBike.new(:type_code => :rigid, :tire_width => 2.5)
+    expect(bike.off_road_ability).to eq 1.5
+    expect(bike.price).to eq 315.0
+  end
+
+  it 'creates a front suspension bike with tire width 2 and front fork travel 3' do
+    bike = MountainBike.new(:type_code => :front_suspension, :tire_width => 2,
+                             :front_fork_travel => 3)
+    expect(bike.off_road_ability).to eq 2.7
+    expect(bike.price).to eq 515.0
+  end
+end
 
 # # First
 # describe 'MountainBike' do
@@ -33,18 +33,18 @@ require_relative '../classes/mountain_bike'
 #   end
 # end
 
-# Second and Third
-describe 'MountainBike' do
-  it 'creates a rigid bike with a 2.5 tire width' do
-    bike = RigidMountainBike.new(:type_code => :rigid, :tire_width => 2.5)
-    expect(bike.off_road_ability).to eq 1.5
-    expect(bike.price).to eq 315.0
-  end
 
-  it 'creates a front suspension bike with tire width 2 and front fork travel 3' do
-    bike = FrontSuspensionMountainBike.new(:type_code => :front_suspension, :tire_width => 2,
-                                           :front_fork_travel => 3)
-    expect(bike.off_road_ability).to eq 2.7
-    expect(bike.price).to eq 515.0
-  end
-end
+# # Second and Third
+# describe 'MountainBike' do
+#   it 'creates a rigid bike with a 2.5 tire width' do
+#     bike = RigidMountainBike.new(:tire_width => 2.5)
+#     expect(bike.off_road_ability).to eq 1.5
+#     expect(bike.price).to eq 315.0
+#   end
+#
+#   it 'creates a front suspension bike with tire width 2 and front fork travel 3' do
+#     bike = FrontSuspensionMountainBike.new(:tire_width => 2, :front_fork_travel => 3)
+#     expect(bike.off_road_ability).to eq 2.7
+#     expect(bike.price).to eq 515.0
+#   end
+# end
